2684e44bb1ea6a43d124db3dbde60b72f93e356b
y2j855 <y2j855@hotmail.com>
2020-09-16 15:24:44 +0800

    delete invalid code

--- src/com/tony/tank/ResourceManager.java
+++ src/com/tony/tank/ResourceManager.java
@@ -27,23 +27,6 @@ public class ResourceManager {
             goodTankL = ImageUtil.rotateImage(goodTankU,-90);
             goodTankR = ImageUtil.rotateImage(goodTankU,90);
 
-//            goodTankU2 = ImageIO.read(com.tony.tank.ResourceManager.class.getClassLoader().getResourceAsStream("images/GoodTank2.png"));
-//            goodTankD2 = ImageUtil.rotateImage(goodTankU2,180);
-//            goodTankL2 = ImageUtil.rotateImage(goodTankU2,-90);
-//            goodTankR2 = ImageUtil.rotateImage(goodTankU2,90);
-//
-//            goodTankU[0] = goodTankU1;
-//            goodTankU[1] = goodTankU2;
-//
-//            goodTankD[0] = goodTankD1;
-//            goodTankD[1] = goodTankD2;
-//
-//            goodTankL[0] = goodTankL1;
-//            goodTankL[1] = goodTankL2;
-//
-//            goodTankR[0] = goodTankR1;
-//            goodTankR[1] = goodTankR2;
-
             badTankU = ImageIO.read(com.tony.tank.ResourceManager.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
             badTankD = ImageUtil.rotateImage(badTankU,180);
             badTankL = ImageUtil.rotateImage(badTankU,-90);
