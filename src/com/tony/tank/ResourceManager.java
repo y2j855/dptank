c61c713dabfa4563626ed034748bcc055ca7dfc2
y2j855 <y2j855@hotmail.com>
2020-09-16 15:09:10 +0800

    test change tanke image

--- src/com/tony/tank/ResourceManager.java
+++ src/com/tony/tank/ResourceManager.java
@@ -14,6 +14,7 @@ public class ResourceManager {
     private ResourceManager(){}
 
     public static BufferedImage goodTankU, goodTankD, goodTankL, goodTankR;
+
     public static BufferedImage badTankU, badTankD, badTankL, badTankR;
     public static BufferedImage bulletU,bulletD,bulletL,bulletR;
     public static BufferedImage[] explodes = new BufferedImage[16];
@@ -25,6 +26,23 @@ public class ResourceManager {
             goodTankD = ImageUtil.rotateImage(goodTankU,180);
             goodTankL = ImageUtil.rotateImage(goodTankU,-90);
             goodTankR = ImageUtil.rotateImage(goodTankU,90);
+
+//            goodTankU2 = ImageIO.read(com.tony.tank.ResourceManager.class.getClassLoader().getResourceAsStream("images/GoodTank2.png"));
+//            goodTankD2 = ImageUtil.rotateImage(goodTankU2,180);
+//            goodTankL2 = ImageUtil.rotateImage(goodTankU2,-90);
+//            goodTankR2 = ImageUtil.rotateImage(goodTankU2,90);
+//
+//            goodTankU[0] = goodTankU1;
+//            goodTankU[1] = goodTankU2;
+//
+//            goodTankD[0] = goodTankD1;
+//            goodTankD[1] = goodTankD2;
+//
+//            goodTankL[0] = goodTankL1;
+//            goodTankL[1] = goodTankL2;
+//
+//            goodTankR[0] = goodTankR1;
+//            goodTankR[1] = goodTankR2;
 
             badTankU = ImageIO.read(com.tony.tank.ResourceManager.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
             badTankD = ImageUtil.rotateImage(badTankU,180);
