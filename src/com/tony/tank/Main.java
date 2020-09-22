6dab3e3b1566b037ef0edfab7fcbabb7ea1ff50f
y2j855 <y2j855@hotmail.com>
2020-09-22 15:54:55 +0800

    view layer and model layer separate

--- src/com/tony/tank/Main.java
+++ src/com/tony/tank/Main.java
@@ -10,6 +10,11 @@ import com.tony.tank.facade.GameModel;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
+        int initTankCount = Integer.parseInt((String)PropertyManager.getInstance().get("initTankCount"));
+        GameModel gameModel = GameModel.getInstance();
+        for (int i = 0; i < initTankCount; i++) {
+            gameModel.enemyTanks.add(new Tank(50 + i*80,200,Direction.DOWN,Group.BAD,gameModel));
+        }
 
         while (true){
             Thread.sleep(50);
--- src/com/tony/tank/facade/GameObject.java
+++
@@ -1,9 +0,0 @@
-package com.tony.tank.facade;
-
-/**
- * @author: Tony.Chen
- * Create Time : 2020/9/22 15:45
- * Description:
- */
-public class GameObject {
-}
